a22a0313ad57b98d58267f5fd7436d59784f2dcd
Akshit Kumar <kumakshit15@gmail.com>
2023-08-18 10:41:56 +0530

    Ignoring .env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <>
-      <h1 className="text-blue-500">Movie  Kumar</h1>
+      <h1 className="text-blue-500">Movie Kumar</h1>
+      <h1 className="text-blue-500"> {import.meta.env.VITE_TMDB_API_KEY} </h1>
     </>
   );
 }
